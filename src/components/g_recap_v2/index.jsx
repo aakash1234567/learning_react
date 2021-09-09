f07a92b3f5e8a9089cf372c049092691bf2867b3
aakash1234567 <aakashkhandelwal56@gmail.com>
2021-09-09 16:05:58 +0530

    updates

--- src/components/g_recap_v2/index.jsx
+++ src/components/g_recap_v2/index.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 const GRecaptcha = () => {
 	const SITE_KEY = "6LeLv1UcAAAAAJ6rMQ_f_MRZCta1fzQsbnecqEoX";
+	const [recap, setRecap] = useState(false);
 	useEffect(() => {
 		const loadScriptByURL = (id, url, callback) => {
 			const isScriptExist = document.getElementById(id);
@@ -22,23 +23,37 @@ const GRecaptcha = () => {
 		// load the script by passing the URL
 		loadScriptByURL(
 			"recaptcha-key",
-			`https://www.google.com/recaptcha/api.js`,
+			`https://www.google.com/recaptcha/api.js?render=explicit`,
 			function () {
 				console.log("Script loaded!");
-				// setRecap(true);
+				setRecap(true);
 			}
 		);
 	}, []);
+	if (recap) {
+		const check = () => {
+			console.log("ok");
+			const token = window.grecaptcha.getResponse();
+			console.log(token);
+		};
+		window.grecaptcha.ready(() => {
+			window.grecaptcha.render("gcap", {
+				sitekey: SITE_KEY,
+				callback: check,
+				theme: "dark",
+			});
+		});
+		setRecap(false);
+	}
 	return (
 		<>
 			<h1>Recaptcha</h1>
-			<form action="?" method="POST">
-				<div className="g-recaptcha" data-sitekey={SITE_KEY}></div>
-				<br />
-				<input type="submit" value="Submit" />
-			</form>
+			<div id="gcap"></div>
 		</>
 	);
 };
 
 export default GRecaptcha;
+
+// 6LeLv1UcAAAAAJ6rMQ_f_MRZCta1fzQsbnecqEoX
+// 6LeLv1UcAAAAAK2Qo5bpEM3fVHefgdVHef7IIrDQ
